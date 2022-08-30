873903d8e8f9ced176d9ebef3ee0da17fabe0cd0
Jeshurungamer <106802924+Jeshurungamer@users.noreply.github.com>
2022-08-30 21:12:06 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,6 @@
+var player1_name=localStorage.getItem("player1_name_input");
+var player2_name=localStorage.getItem("player2_name_input");
+document.getElementById("player1_name").innerHTML=player1_name;
+document.getElementById("player2_name").innerHTML=player2_name;
+var player1_score=0;
+var player2_score=0;
